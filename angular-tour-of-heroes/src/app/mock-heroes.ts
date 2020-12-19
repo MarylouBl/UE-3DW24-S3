fd862a139abee9fb7a1d7e51c62416c7e371206a
Blondin Marylou <blondin.marylou@etu.unilim.fr>
2020-12-19 11:28:04 +0100

    Étape 2 - Tuto Angular

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Onodera Punpun' },
+  { id: 12, name: 'Tanaka Aiko' },
+  { id: 13, name: 'Guts' },
+  { id: 14, name: 'Gojô Satoru' },
+  { id: 15, name: 'Elias Ainsworth' },
+  { id: 16, name: 'Legion' },
+  { id: 17, name: 'Dabi' },
+  { id: 18, name: 'Aurelion Sol' },
+  { id: 19, name: 'Amaterasu' },
+  { id: 20, name: 'Cypher' }
+];
